2f0a5283853ca69325b452e04c553d3e68e8a008
Florencia Basso <florenciavbasso@gmail.com>
2023-10-25 22:20:04 -0300

    desafio 9

--- src/repositories/productRepository.js
+++ src/repositories/productRepository.js
@@ -1,9 +1,39 @@
-import Product from '../models/Product';
+import Product from '../models/Product.js';
 
 class ProductRepository {
-  static async getProductById(productId) {
-    return Product.getProductById(productId);
-  }
+    async getProductById(productId) {
+        try {
+            return await Product.findById(productId);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getAllProducts() {
+        try {
+            return await Product.find().exec();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateProduct(productId, productData) {
+        try {
+            return await Product.findByIdAndUpdate(productId, productData, {
+                new: true
+            }).exec();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async decreaseStock(productId, quantity) {
+        try {
+            // Implementa la lógica para reducir la cantidad de stock de un producto específico aquí
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 export default ProductRepository;
